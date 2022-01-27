f6ec9228653134279923bc2ddb09a255be0f8ea4
Octavio Villena <octaviovillena@hotmail.com>
2022-01-26 16:01:57 -0800

    send status 200 if data accepted

--- routes/entries.js
+++ routes/entries.js
@@ -4,5 +4,5 @@ const inputValidation = require('../middleware/inputValidation');
 
 router.post('/add', inputValidation.validateInput, (req, res) => {
   //middleware validates input data. If all good then spits out this message
-  res.send('data looks acceptable!');
+  res.status(200).send('data looks acceptable!');
 });
