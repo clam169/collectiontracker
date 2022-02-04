ed81b1b4d4bc54a6bd595cb3f22b23e87067806a
Octavio Villena <octaviovillena@hotmail.com>
2022-02-04 01:17:57 -0800

    get sources and delete is delete

--- database.js
+++ database.js
@@ -20,6 +20,21 @@ module.exports = async function () {
     return result.rows;
   }
 
+  async function getSources(postData, callback) {
+    let sqlQuery = `SELECT cx_source.source_id, name FROM public.cx_source
+    JOIN source ON cx_source.source_id = source.source_id
+    WHERE cx_account_id = $1;`;
+    console.log(sqlQuery, '$1 is ', postData.body.account_id);
+    client.query(sqlQuery, [postData.body.account_id], (err, result) => {
+      if (err) {
+        callback(err, null);
+      }
+      console.log('--------------------------------');
+      console.log(result);
+      callback(null, result.rows);
+    });
+  }
+
   async function deleteEntry(postData, callback) {
     let sqlQuery = `DELETE FROM entry WHERE entry_id = $1;`;
     console.log(sqlQuery, '$1 is ', postData.body.entry_id);
@@ -35,6 +50,7 @@ module.exports = async function () {
 
   return {
     testQuery,
+    getSources,
     deleteEntry,
   };
 };
