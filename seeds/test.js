fcdbb8ca84cd21f1916c217eba710a93916fb793
Ovillena <44690943+Ovillena@users.noreply.github.com>
2022-02-07 21:22:11 -0800

    Add files via upload

---
+++ seeds/test.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('things')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex.raw(`
+      INSERT INTO things (username)
+      values ('sam'),
+      ('test tickles'),
+      ('jo jo'),
+      ('Megan'),
+      ('sara'),
+      ('ninja');
+      `);
+    });
+};
